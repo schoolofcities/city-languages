8e4cfe922c567e42186e248d97459e27d8274521
an-k45 <an_k27@protonmail.com>
2025-11-27 14:43:00 -0500

    Outline page, import data and libraries

---
+++ src/routes/toronto/+page.svelte
@@ -0,0 +1,90 @@
+<script>
+	import Logo from '$lib/header-footer/LogoTop.svelte';
+	import TitleStandard from '$lib/header-footer/TitleStandard.svelte';
+	import ImageSingle from '$lib/images-videos/ImageSingle.svelte';
+	import AuthorDate from '$lib/header-footer/AuthorDate.svelte';
+	import Footer from '$lib/header-footer/Footer.svelte';
+	import * as Plot from "@observablehq/plot";
+	
+	// Load the JSON data files
+	import data1971 from '../../data/num_speakers_centroid_1971.json';
+	import data1996 from '../../data/num_speakers_centroid_1996.json';
+	import data2021 from '../../data/num_speakers_centroid_2021.json';
+	
+	// Make data available for reactive statements
+	let datasets = $state({
+		1971: data1971,
+		1996: data1996,
+		2021: data2021
+	});
+</script>
+
+
+<main>
+	<Logo logoType="Blue" backgroundColor="var(--brandWhite)"/>
+
+	<ImageSingle
+		imageURL="https://jamaps.github.io/photos/picimgs/taipei1_2025.jpg"
+		caption="Taipei skyline."
+		source="Jeff Allen."
+		altText=""
+		maxWidth="1080px"
+	/>
+
+
+	<TitleStandard
+		title="Fun Captivating Project Title"
+		subtitle="Maybe a slightly longer more detailed wordier project subtitle "
+	/>
+
+	<div class="text">
+
+		<AuthorDate
+			authors="<a href='' target='_blank'>Author Name</a> & <a href='' target='_blank'>Author Name</a>"
+			date="July 2025"
+		/>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat lacus eu dolor dapibus sodales. Aenean venenatis metus id eleifend tincidunt. Nulla ut lacus et urna finibus bibendum sit amet et ante. Aliquam tristique, ex sed porttitor hendrerit, ex odio accumsan ex, eu maximus leo quam quis nulla. 
+		</p>
+
+		<p>
+			Cras tincidunt nisi non tempus suscipit. Nullam metus erat, ultrices vitae mauris commodo, placerat sollicitudin sem. In vitae dignissim eros. Phasellus porttitor orci. nisl, vitae iaculis nulla pretium et. Fusce nec tortor erat. Vestibulum pretium nisl et ligula ultrices fringilla.
+		</p>
+
+		<p>
+			Vivamus non finibus erat. Ut quis mi at felis aliquam rhoncus eu eget augue. Nunc convallis, dui et congue suscipit, nisl sapien malesuada ligula, vitae luctus justo ligula finibus diam. Quisque aliquam et lacus vitae venenatis. Duis id vulputate augue, vel posuere ex. Nam fermentum consequat dolor, ac finibus justo finibus sit amet. Nam suscipit egestas tellus, malesuada dignissim neque dignissim sed. />
+		</p>
+
+		<p>
+			Nunc vel massa turpis. Vivamus id odio ut nulla dignissim molestie. 
+		</p>
+
+	</div>
+
+    <div class="text">
+        <div class="details">
+
+			<h2>
+				Data & Methodology
+			</h2>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consequat lacus eu dolor dapibus sodales. Aenean venenatis metus id eleifend tincidunt. Nulla ut lacus et urna finibus bibendum sit amet et ante. Aliquam tristique, ex sed porttitor hendrerit, ex odio accumsan ex, eu maximus leo quam quis nulla. 
+			</p>
+			<p>
+				Cras tincidunt nisi non tempus suscipit. Nullam metus erat, ultrices vitae mauris commodo, placerat sollicitudin sem. In vitae dignissim eros. Phasellus porttitor orci nisl, vitae iaculis nulla pretium et. Fusce nec tortor erat. Vestibulum pretium nisl et ligula ultrices fringilla. 
+			</p>
+			<p>
+				Vivamus non finibus erat. Ut quis mi at felis aliquam rhoncus eu eget augue. Nunc convallis, dui et congue suscipit, nisl sapien malesuada ligula, vitae luctus justo ligula finibus diam. Quisque aliquam et lacus vitae venenatis. Duis id vulputate augue, vel posuere ex. Nam fermentum consequat dolor, ac finibus justo finibus sit amet. Nam suscipit egestas tellus, malesuada dignissim neque dignissim sed. 
+			</p>
+
+		</div>
+    </div>
+    
+	<Footer />
+</main>
+
+
+<style>
+    
+</style>
